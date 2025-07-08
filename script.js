e66e730d6552fffa4899f3d961713e4e9db217e1
Bernardo Garcia <bernardogarciagarcia441@gmail.com>
2025-07-08 01:22:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ function createTrackCard(index) {
   preview.controls = true;
   preview.style.display = 'block'; // always visible!
   preview.style.background = "#000";
-  preview.style.width = '60%';
+  preview.style.width = '40%';
   preview.style.marginTop = '6px';
   preview.autoplay = false; // Do not autoplay on uploaded videos!
   preview.muted = true;
